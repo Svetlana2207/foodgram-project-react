9191eff897a0e1f66ef91011ff3512a87a0035d4
Svetlana Ponomareva <s-ponomareva@mail.ru>
2022-08-23 00:17:46 +0500

    7 recipes test

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,99 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.conf import settings
-# from django.db import models
-
-
-# class User(AbstractUser):
-
-#     CHOICE_ROLE = (
-#         ('user', 'user'),
-#         ('moderator', 'moderator'),
-#         ('admin', 'admin'),
-#     )
-#     email = models.EmailField(
-#         verbose_name='Адрес электронной почты',
-#         max_length=254,
-#         unique=True,
-#     )
-#     username = models.SlugField(
-#         verbose_name='Имя пользователя',
-#         max_length=150,
-#         unique=True,
-#     )
-#     first_name = models.CharField(
-#         'Имя',
-#         max_length=150,
-#         blank=True,
-#     )
-#     last_name = models.CharField(
-#         'Фамилия',
-#         max_length=150,
-#         blank=True
-#     )
-#     role = models.TextField(
-#         'Пользовательская роль',
-#         max_length=40,
-#         choices=CHOICE_ROLE,
-#         default='user',
-#         blank=True,
-#     )
-#     password = models.CharField(
-#         'Пароль',
-#         max_length=150,
-#         null=True,
-#         blank=False,
-#     )
-
-#     @property
-#     def is_admin(self):
-#         return self.role == 'admin'
-
-#     @property
-#     def is_moderator(self):
-#         return self.role == 'moderator'
-
-#     @property
-#     def is_user(self):
-#         return self.role == 'user'
-
-#     class Meta:
-#         ordering = ['-id']
-
-#     def __str__(self):
-#         return self.username
-
-
-# class UserSubscription(models.Model):
-#     subscriber = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         related_name='subscriptions',
-#         on_delete=models.CASCADE
-#     )
-#     subscription = models.ForeignKey(
-#         settings.AUTH_USER_MODEL,
-#         related_name='subscribers',
-#         on_delete=models.CASCADE
-#     )
-#     subscribed_at = models.DateTimeField(
-#         auto_now_add=True,
-#         verbose_name='subscription date'
-#     )
-
-#     class Meta:
-#         constraints = [
-#             models.UniqueConstraint(
-#                 fields=['subscriber', 'subscription'],
-#                 name='unique_subscription',
-#             )
-#         ]
-#         verbose_name = 'Subscription'
-#         verbose_name_plural = 'Subscriptions'
-#         ordering = ['-subscribed_at', 'id']
-
-#     def __str__(self):
-#         return f'{self.subscriber} - {self.subscription}'
-
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -111,9 +15,12 @@ class User(AbstractUser):
     last_name = models.CharField(
         max_length=150, verbose_name='Фамилия'
     )
-
+    password = models.CharField(
+        verbose_name='Пароль',
+        max_length=150, null=True, blank=False,
+    )
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = []
 
     class Meta:
         ordering = ['-id']
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,20 +1,3 @@
-# from django.contrib.auth.tokens import default_token_generator
-# from django.core.mail import send_mail
-# from django.shortcuts import get_object_or_404
-# from rest_framework import status, viewsets
-# from rest_framework.decorators import action
-# from rest_framework.filters import SearchFilter
-# from rest_framework.pagination import PageNumberPagination
-# from rest_framework.permissions import AllowAny, IsAuthenticated
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from rest_framework_simplejwt.tokens import RefreshToken
-
-# from .models import User
-# from .permissions import IsAdmin
-# from .serializers import (UserSerializer)
-
-
 from rest_framework import status
 from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
