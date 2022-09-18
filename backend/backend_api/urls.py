05f3ec71a1f51caee21246aa3105963470c4b6d4
Svetlana2207 <s-ponomareva@mail.ru>
2022-09-18 13:58:58 -0400

    redoc

--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,10 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
     path('api/', include('users.urls')),
-    path('docs/',
-         TemplateView.as_view(template_name='docs/redoc.html'),
-         name='redoc'
-         ),
 ]
 
 if settings.DEBUG:
